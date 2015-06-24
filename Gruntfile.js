78b046133ba7e0873b59c264afc8dce7864c2096
Sean Kerr <seankerr@Sean.local>
2015-06-24 21:14:43 +1000

    add stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 // Gruntfile.js
 module.exports = function(grunt) {
+
     grunt.registerTask('list', function () {
         var done = this.async();
         grunt.util.spawn({ cmd: 'node', args: ['S3ListBuckets.js'], opts: {stdio: 'inherit'}}, function(e, result) {
@@ -7,6 +8,18 @@ module.exports = function(grunt) {
             done();
         });
     });
+
+    grunt.registerTask('create', function (n) {
+
+        if (n == null) {
+            grunt.log.warn('A bucket name bust be specified');
+
+        } else {
+            var done = this.async();
+            grunt.util.spawn({ cmd: 'node', args: ['S3CreateBucket.js', n], opts: {stdio: 'inherit'}}, function() {
+                done();
+            });
+        }
+    });
+
 };
-
-
